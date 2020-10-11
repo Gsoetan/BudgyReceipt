0ba202ccd3a92871e5d5d08bde204a376f1ff462
Jbsession2 <60483350+Jbsession2@users.noreply.github.com>
2020-10-11 12:30:05 -0400

    Add files via upload

---
+++ src/main/java/com/example/notes/Util.java
@@ -0,0 +1,120 @@
+package com.example.notes;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+
+public class Util {
+
+    //String extra for a note's filename
+    public static final String EXTRAS_NOTE_FILENAME = "EXTRAS_NOTE_FILENAME";
+    public static final String FILE_EXTENSION = ".bin";
+
+    //Save a note on private storage of the app
+    public static boolean saveNote(Context context, Note note) {
+
+        String fileName = String.valueOf(note.getDateTime()) + FILE_EXTENSION;
+
+        FileOutputStream fos;
+        ObjectOutputStream oos;
+
+        try{
+            fos = context.openFileOutput(fileName, context.MODE_PRIVATE);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(note);
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            //tell user the note was saved!
+            return false;
+        }
+
+        return true;
+    }
+
+    //Read all saved
+
+    static ArrayList<Note> getAllSavedNotes(Context context) {
+        ArrayList<Note> notes = new ArrayList<>();
+
+        File filesDir = context.getFilesDir();
+        ArrayList<String> noteFiles = new ArrayList<>();
+
+        for(String file : filesDir.list()) {
+            if(file.endsWith(FILE_EXTENSION)) {
+                noteFiles.add(file);
+            }
+        }
+
+        //read objects and add to list of notes
+        FileInputStream fis;
+        ObjectInputStream ois;
+
+        for (int i = 0; i < noteFiles.size(); i++) {
+            try{
+                fis = context.openFileInput(noteFiles.get(i));
+                ois = new ObjectInputStream(fis);
+
+                notes.add((Note) ois.readObject());
+                fis.close();
+                ois.close();
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        return notes;
+    }
+
+    // Loads a note file by its name
+
+    public static Note getNoteByFileName(Context context, String fileName) {
+
+        File file = new File(context.getFilesDir(), fileName);
+        if(file.exists() && !file.isDirectory()) { //check if file actually exist
+
+            Log.v("UTIL", "File exist = " + fileName);
+
+            FileInputStream fis;
+            ObjectInputStream ois;
+
+            try { //load the file
+                fis = context.openFileInput(fileName);
+                ois = new ObjectInputStream(fis);
+                Note note = (Note) ois.readObject();
+                fis.close();
+                ois.close();
+
+                return note;
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        } else {
+            return null;
+        }
+    }
+
+    public static boolean deleteFile(Context context, String fileName) {
+        File dirFiles = context.getFilesDir();
+        File file = new File(dirFiles, fileName);
+
+        if(file.exists() && !file.isDirectory()) {
+            return file.delete();
+        }
+
+        return false;
+    }
+}
