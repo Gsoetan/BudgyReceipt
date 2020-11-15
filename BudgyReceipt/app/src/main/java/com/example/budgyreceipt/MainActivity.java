c5cc366a900a6df774e731d18ab0633aea951ecd
Gsoetan <43526261+Gsoetan@users.noreply.github.com>
2020-11-14 23:18:32 -0500

    Updated

--- BudgyReceipt/app/src/main/java/com/example/budgyreceipt/MainActivity.java
+++ BudgyReceipt/app/src/main/java/com/example/budgyreceipt/MainActivity.java
@@ -2,25 +2,31 @@ package com.example.budgyreceipt;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
+// This is also the receipt activity that will store all of the user created receipt entities
 public class MainActivity extends AppCompatActivity implements ReceiptFragment.OnReceiptCreatedListener {
 
-    Button add_item_button;
     private ReceiptDatabase mReceiptdb;
     private ReceiptAdapter mReceiptAdapter;
     private RecyclerView mRecyclerView;
@@ -34,21 +40,15 @@ public class MainActivity extends AppCompatActivity implements ReceiptFragment.O
         mReceiptdb = ReceiptDatabase.getInstance(getApplicationContext());
 
         mRecyclerView = findViewById(R.id.receiptRecyclerView);
+        RecyclerView.LayoutManager gridLayoutManager =
+                new GridLayoutManager(getApplicationContext(), 1);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
 
         //maybe add layout manager for horizontal bars? idk yet
 
-        mReceiptAdapter = new ReceiptAdapter(getReceipts()); // gotta set this
+        mReceiptAdapter = new ReceiptAdapter(getReceipts());
         mRecyclerView.setAdapter(mReceiptAdapter);
 
-
-        // Switch from Main to OverviewActivity
-        add_item_button = (Button)findViewById(R.id.addItem);
-        add_item_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, OverviewActivity.class));
-            }
-            });
     }
 
     @Override
@@ -59,8 +59,64 @@ public class MainActivity extends AppCompatActivity implements ReceiptFragment.O
             receipt.setId(receiptId);
 
             // TODO: add subject to recycler view
-            // TODO: add toast to confirm addition of merchant
+            Toast.makeText(this, "Added " + receiptMerchant, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void addReceiptClick(View view) { // creates the recycler view
+        FragmentManager manager = getSupportFragmentManager();
+        ReceiptFragment frag = new ReceiptFragment();
+        frag.show(manager, "receiptFragment");
+    }
+
+    private List<Receipt> getReceipts() { return mReceiptdb.receiptDao().getReceiptsNew(); } // load the receipts into the activity based off newest additions
+
+    private class ReceiptHolder extends RecyclerView.ViewHolder implements  View.OnClickListener {
+
+        private Receipt mReceipt;
+        private TextView mText_merchant, mText_total;
+
+        public ReceiptHolder(LayoutInflater inflater, ViewGroup parent){
+            super(inflater.inflate(R.layout.recycler_view_items, parent, false));
+            itemView.setOnClickListener(this);
+            mText_merchant = itemView.findViewById(R.id.merchant_text);
+            mText_total = itemView.findViewById(R.id.total_text);
+        }
+
+        public void bind(Receipt receipt) { // make the fragment
+            mReceipt = receipt;
+            mText_merchant.setText(receipt.getMerchant());
+        }
+
+        @Override
+        public void onClick(View v) {
+            // Start the Overview activity with the selected receipt
+            Intent intent = new Intent(MainActivity.this, OverviewActivity.class);
+            intent.putExtra(OverviewActivity.EXTRA_RECEIPT_ID, mReceipt.getId());
+            startActivity(intent);
+        }
+    }
+
+    private class ReceiptAdapter extends RecyclerView.Adapter<ReceiptHolder> {
+
+        private List<Receipt> mReceiptList;
+
+        public ReceiptAdapter(List<Receipt> receipts) { mReceiptList = receipts; }
+
+        @NonNull
+        @Override
+        public ReceiptHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+            return new ReceiptHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ReceiptHolder holder, int position) {
+            holder.bind(mReceiptList.get(position));
+        }
+
+        @Override
+        public int getItemCount() { return mReceiptList.size(); }
     }
 
     //actionbar menu (settings)
--- BudgyReceipt/app/src/main/java/com/example/budgyreceipt/OverviewActivity.java
+++ BudgyReceipt/app/src/main/java/com/example/budgyreceipt/OverviewActivity.java
@@ -1,64 +1,46 @@
 package com.example.budgyreceipt;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.MediaStore;
-import android.util.SparseArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import static com.example.budgyreceipt.MainCalculations.stringParse;
+import java.util.List;
 
 public class OverviewActivity extends AppCompatActivity {
 
-    DatabaseHelper overviews;
-    private EditText oTitleEt, oDateEt, oTotalEt, oSubTotalEt, oPaymentEt;
+    private final int REQUEST_CODE_UPDATE_ENTRY = 1;
+
+    private ReceiptDatabase mReceiptDb;
+    private long mReceiptId;
+    private List<Overview> mOverviewList;
+    private TextView oTitleEt, oDateEt, oTotalEt, oSubTotalEt, oPaymentEt, tag;
     private ImageView oPhotoIv;
-    ImageButton go_back, save_data;
-    Spinner tags;
+    private FloatingActionButton edit;
 
-    private static final int CAMERA_REQUEST_CODE = 200;
-    private static final int STORAGE_REQUEST_CODE = 400;
-    private static final int IMAGE_PICK_GALLERY_CODE = 1000;
-    private static final int IMAGE_PICK_CAMERA_CODE = 1001;
-
-    String[] cameraPermission;
-    String[] storagePermission;
-    String[] entries;
-
-    Uri image_uri; // image source
+    public static final String EXTRA_RECEIPT_ID = "com.example.budgyreceipt.receipt_id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        // MainActivity should've provided the receipt id of the overview to display
+        Intent intent = getIntent();
+        mReceiptId = intent.getLongExtra(EXTRA_RECEIPT_ID, -1);
+
+        // get the entry data for this overview
+        mReceiptDb = ReceiptDatabase.getInstance(getApplicationContext());
+        mOverviewList = mReceiptDb.overviewDao().getOverviews(mReceiptId);
 
         oTitleEt = findViewById(R.id.merchant);
         oDateEt = findViewById(R.id.date);
@@ -66,214 +48,45 @@ public class OverviewActivity extends AppCompatActivity {
         oSubTotalEt = findViewById(R.id.subTotal);
         oPaymentEt = findViewById(R.id.payment);
         oPhotoIv = findViewById(R.id.photo);
+        tag = findViewById(R.id.tag); //https://developer.android.com/guide/topics/ui/controls/spinner.html
 
-        overviews = new DatabaseHelper(this);
-
-        save_data = findViewById(R.id.saveButton);
-        save_data.setOnClickListener(new View.OnClickListener() {
+        edit = findViewById(R.id.editText);
+        edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseHelper databaseHelper = new DatabaseHelper(OverviewActivity.this);
-
-                String titleEt = oTitleEt.getText().toString();
-                String dateEt = oDateEt.getText().toString();
-                String totalEt = oTotalEt.getText().toString();
-                String subtotalEt = oSubTotalEt.getText().toString();
-                String tagSelection = tags.getSelectedItem().toString();
-                String paymentEt = oPaymentEt.getText().toString();
-                if (oTitleEt.length() != 0 || oDateEt.length() != 0 || oTotalEt.length() != 0 || oSubTotalEt.length() != 0 || oPaymentEt.length() != 0){ // check to make sure at least one entry is filled
-                    addData(titleEt, dateEt, totalEt, subtotalEt, tagSelection, paymentEt);
-                    oTitleEt.setText("");
-                    oDateEt.setText("");
-                    oTotalEt.setText("");
-                    oSubTotalEt.setText("");
-                    tags.setSelection(0);
-                    oPaymentEt.setText("");
-                    oPhotoIv.setImageURI(null); //not working yet
-                } else {
-                    Toast.makeText(OverviewActivity.this, "At least one field must be filled.", Toast.LENGTH_LONG).show();
-                }
+                editOverview();
             }
         });
 
-        go_back = findViewById(R.id.backButton);
-        go_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(OverviewActivity.this, MainActivity.class));
-                finish();
-            }
-        });
-
-        //camera permissions
-        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-        //storage permission
-        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
-        tags = findViewById(R.id.tag); //https://developer.android.com/guide/topics/ui/controls/spinner.html
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tags, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        tags.setAdapter(adapter);
     }
 
-    // https://developer.android.com/guide/topics/ui/settings/use-saved-values
-    // https://developer.android.com/topic/libraries/architecture/saving-states
-    public void addData(String title, String date, String total, String subtotal, String tag, String payment) {
-        Boolean insertData = overviews.addData(title, date, total, subtotal, tag, payment);
-
-        if (insertData == true) {
-            Toast.makeText(OverviewActivity.this, "Entry saved", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(OverviewActivity.this, "Oops! Something went wrong.", Toast.LENGTH_LONG).show();
-        }
+    private void editOverview() {
+        Intent intent = new Intent(this, OverviewEditActivity.class);
+        intent.putExtra(EXTRA_RECEIPT_ID, mReceiptId);
+        long overviewId = mOverviewList.get(0).getId();
+        intent.putExtra(OverviewEditActivity.EXTRA_OVERVIEW_ID, overviewId);
+        startActivityForResult(intent, REQUEST_CODE_UPDATE_ENTRY);
     }
 
-    public void addPhoto(View view) {
-        showImageImportDialog();
-    }
-
-    private void showImageImportDialog(){
-        //items to display in dialog
-        String [] items = {"Camera", "Gallery"};
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        // Set title
-        dialog.setTitle("Select source");
-        dialog.setItems(items, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if (which == 0) {
-                    //camera option clicked
-                    checkCameraPermission(); //check if camera permission allowed
-                    pickCamera();
-                }
-                if (which == 1) {
-                    //gallery option clicked
-                    checkStoragePermission(); //check if storage permission allowed
-                    pickGallery();
-                }
-            }
-        });
-        dialog.create().show(); // show dialog
-    }
-
-    private void pickGallery() {
-        //intent to pick image from gallery
-        Intent intent = new Intent(Intent.ACTION_PICK); //
-        //set intent to open up your images
-        intent.setType("image/*");
-        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
-    }
-
-    private void pickCamera() {
-        ContentValues values = new ContentValues(); // https://developer.android.com/guide/topics/providers/content-providers
-        values.put(MediaStore.Images.Media.TITLE, "New Picture"); //title of picture
-        values.put(MediaStore.Images.Media.DESCRIPTION, "Image converted to text"); // description
-        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
-        startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_CODE);
-    }
-
-    private void checkStoragePermission() { // check to see if user has granted application access to photos on device
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != (PackageManager.PERMISSION_GRANTED)) {
-            ActivityCompat.requestPermissions(this, storagePermission, STORAGE_REQUEST_CODE);
-        }
-        return;
-    }
-
-    private void checkCameraPermission() { // check to see if user has granted permission for application to take photos using camera as well as permission to save it (more for camera option)
-        boolean camPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
-        boolean writePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
-        if (camPermission != writePermission){
-            ActivityCompat.requestPermissions(this, cameraPermission, CAMERA_REQUEST_CODE);
-        }
-        return;
-    }
-
-    //handle the permission request result
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) { // https://stackoverflow.com/questions/52502453/import-image-from-camera-or-gallery-app-crash
-        switch (requestCode) {
-            case CAMERA_REQUEST_CODE:
-                if(grantResults.length > 0){
-                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    if (cameraAccepted && writeStorageAccepted) {
-                        pickCamera();
-                    } else {
-                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                break;
-            case STORAGE_REQUEST_CODE:
-                if(grantResults.length > 0){
-                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    if (writeStorageAccepted) {
-                        pickGallery();
-                    } else {
-                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                break;
-        }
-    }
-
-    //handle image result
-    @SuppressLint("MissingSuperCall")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
-                //got image from gallery, now crop it
-                CropImage.activity(data.getData())
-                        .setGuidelines(CropImageView.Guidelines.ON) // enable image guidelines
-                        .start(this);
-            }
-            if (requestCode == IMAGE_PICK_CAMERA_CODE) {
-                //got image from camera, now crop it
-                CropImage.activity(image_uri)
-                        .setGuidelines(CropImageView.Guidelines.ON) // enable image guidelines
-                        .start(this);
-            }
-        }
-        //get cropped image
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK) {
-                Uri resultUri = result.getUri(); //get image uri
-                oPhotoIv.setImageURI(resultUri); //set image to image view
+        super.onActivityResult(requestCode, resultCode, data);
 
-                //make drawable bitmap for text recognition
-                BitmapDrawable bitmapDrawable = (BitmapDrawable) oPhotoIv.getDrawable();
-                Bitmap bitmap = bitmapDrawable.getBitmap();
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_UPDATE_ENTRY) {
 
-                TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+            // get updated overview
+            long overviewId = data.getLongExtra(OverviewEditActivity.EXTRA_OVERVIEW_ID, -1);
+            Overview updatedOverview = mReceiptDb.overviewDao().getOverview(overviewId);
 
-                if (!recognizer.isOperational()) { // if there was a problem with the recognizer return an error
-                    Toast.makeText(this, "there was an error", Toast.LENGTH_SHORT).show();
-                } else {
-                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-                    SparseArray<TextBlock> items = recognizer.detect(frame);
-                    StringBuilder sb = new StringBuilder();
-                    //get text for sb until there is nothing left to read
-                    for (int i = 0; i < items.size(); i++) {
-                        TextBlock myItem = items.valueAt(i);
-                        sb.append(myItem.getValue());
-                        sb.append("\n");
-                    }
-                    //set all entries to their respective values
-                    entries = stringParse(sb); //returns a array in format {date, payment, total, subtotal}
-                    oDateEt.setText(entries[0]);
-                    oPaymentEt.setText(entries[1]);
-                    oTotalEt.setText(entries[2]);
-                    oSubTotalEt.setText(entries[3]);
-                }
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) { // if there is a problem with the crop tool
-                //if there is any error show it
-                Exception error = result.getError();
-                Toast.makeText(this, "" + error, Toast.LENGTH_SHORT).show();
-            }
+            Overview currentOverview = mOverviewList.get(0);
+            currentOverview.setDate(updatedOverview.getDate());
+            currentOverview.setTotal(updatedOverview.getTotal());
+            currentOverview.setSubtotal(updatedOverview.getSubtotal());
+            currentOverview.setTag(updatedOverview.getTag());
+            currentOverview.setPayment(updatedOverview.getPayment());
+            // grab img
+
+            Toast.makeText(this, "Entries updated", Toast.LENGTH_SHORT).show();
         }
     }
 }
